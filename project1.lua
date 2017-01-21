d518bd5b5007833d168264d6e62aaa346ac6473f
logan <rs>
2017-01-21 16:36:58 -0700

    Create project file

---
+++ project1.lua
@@ -0,0 +1,29 @@
+--[[
+
+  * * * * lip.lua * * * *
+
+Lua image processing program: exercise all LuaIP library routines.
+
+Authors: John Weiss and Alex Iverson
+Class: CSC442/542 Digital Image Processing
+Date: Spring 2017
+
+--]]
+
+-- LuaIP image processing routines
+require "ip"   -- this loads the packed distributable
+local viz = require "visual"
+local il = require "il"
+
+
+-----------
+-- menus --
+-----------
+
+imageMenu("Point processes", {})
+
+imageMenu("Histogram processes", {})
+
+imageMenu("Neigborhood ops", {})
+
+start()
